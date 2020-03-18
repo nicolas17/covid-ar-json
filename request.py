69cecfaa997fb446e9d8c095550cd7f95f68c5e9
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-03-17 21:23:23 -0300

    Add code to request and parse PDF list, and to download PDFs

---
+++ request.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2020 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_pdf_urls():
+    resp = requests.get("https://www.argentina.gob.ar/coronavirus/informe-diario", headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    for elem in soup.find('div', class_='downloads').find_all('a'):
+        yield elem["href"]
+
+def download_file(url, fd):
+    resp = requests.get(url, headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
+    for chunk in resp.iter_content(chunk_size=4096):
+        fd.write(chunk)
+    fd.flush()
