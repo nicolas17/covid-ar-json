3d96ad075a8ec394391d49e76e563e55d3bdd1a6
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-03-18 01:14:57 -0300

    Convert main.py into an AWS Lambda handler

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import tempfile
 import json
 import logging
+import boto3
 
 import request
 import pdfconvert
@@ -14,16 +15,33 @@ import textparser
 
 logging.basicConfig(level=logging.INFO)
 
-logging.info("Downloading list")
-pdf_urls = list(request.get_pdf_urls())
-pdf_url = pdf_urls[0]
-logging.info("PDF URL: %s", pdf_url)
-with tempfile.NamedTemporaryFile(suffix='.pdf') as f:
-    request.download_file(pdf_url, f)
-    logging.info("File downloaded into %s", f.name)
-    text = pdfconvert.text_from_pdf(f.name)
-    logging.info("Converted into %d bytes of text", len(text))
+def handler(event, context):
+    logging.info("Downloading list")
+    pdf_urls = list(request.get_pdf_urls())
+    pdf_url = pdf_urls[0]
+    logging.info("PDF URL: %s", pdf_url)
+    with tempfile.NamedTemporaryFile(suffix='.pdf') as f:
+        request.download_file(pdf_url, f)
+        logging.info("File downloaded into %s", f.name)
+        text = pdfconvert.text_from_pdf(f.name)
+        logging.info("Converted into %d bytes of text", len(text))
 
-report = textparser.parse(text)
-output = {'cases': report.cases, 'deaths': report.deaths, 'source_url': pdf_url}
-print(json.dumps(output))
+    report = textparser.parse(text)
+    output = {'cases': report.cases, 'deaths': report.deaths, 'source_url': pdf_url, 'blah': 42}
+    output_json = json.dumps(output).encode('utf8')
+
+    s3 = boto3.client('s3')
+    logging.info("Getting current file from S3")
+    current_obj = s3.get_object(Bucket='nicolas17', Key='covid-ar.json')
+    current_data = current_obj['Body'].read()
+    if current_data == output_json:
+        logging.info("File is already up to date")
+    else:
+        logging.info("Uploading to S3")
+        s3.put_object(Bucket='nicolas17', Key='covid-ar.json', ACL='public-read', ContentType='application/json', Body=output_json)
+        logging.info("Done!")
+
+    return {'result': output_json}
+
+if __name__ == '__main__':
+    print(handler({},{}))
