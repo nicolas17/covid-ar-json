a269e2ebf388599e8b7a41754f5e53cb0a833722
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-03-19 02:21:56 -0300

    Use a requests.session to reuse the connection for html and pdf downloads

--- request.py
+++ request.py
@@ -5,14 +5,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+sess = requests.session()
+
 def get_pdf_urls():
-    resp = requests.get("https://www.argentina.gob.ar/coronavirus/informe-diario", headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
+    resp = sess.get("https://www.argentina.gob.ar/coronavirus/informe-diario", headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
     soup = BeautifulSoup(resp.content, 'html.parser')
     for elem in soup.find('div', class_='downloads').find_all('a'):
         yield elem["href"]
 
 def download_file(url, fd):
-    resp = requests.get(url, headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
+    resp = sess.get(url, headers={'User-Agent': 'CovidParser/0.1 (+nicolas.alvarez+covid@gmail.com)'})
     for chunk in resp.iter_content(chunk_size=4096):
         fd.write(chunk)
     fd.flush()
