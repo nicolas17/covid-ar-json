aa0ede6e9bac2f15464be59fbd46bb37afaaf2bb
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-03-21 20:37:20 -0300

    Support running pdftotext both from cwd and from /usr/bin

--- pdfconvert.py
+++ pdfconvert.py
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: MIT
 
 import subprocess
+import os
+
+os.environ['PATH'] = ".:"+os.environ['PATH']
 
 def text_from_pdf(filename):
-    proc = subprocess.run(['./pdftotext', filename, '-'], capture_output=True)
+    proc = subprocess.run(['pdftotext', filename, '-'], capture_output=True)
     return proc.stdout.decode('utf8')
