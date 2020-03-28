c2b95ef434690af6016e3afc6d0f7c0da9db9c1e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-03-27 22:52:49 -0300

    Refactor parsing of main numbers (cases, deaths)
    This looks like it has more duplicated code, since the int(coalesce(...))
    stuff now appears again for each alternative regex. But it will make it
    easier when it's not a single regex getting both numbers.

--- textparser.py
+++ textparser.py
@@ -22,26 +22,32 @@ def num_regex(capture_name):
     # handles '45' and 'cuarenta y cinco (45)' and 'veintiún (21)'
     return r'((?P<{0}1>\d+)|[a-záéíóú ]+ \((?P<{0}2>\d+)\))'.format(capture_name)
 
+def parse_main_totals(text, report):
+    m = re.search('El total de casos confi?rmados en Argenti?na (a las \d+ horas de hoy )?es de '+num_regex('c')+'( casos)?(\*|\(*\)|, \(\*\))?, (de los cuales )?'+num_regex('d')+' (fallecieron|falleció)', text)
+    if m:
+        logging.info("Matched sentence: '{}'".format(m.group(0)))
+        report.cases = int(coalesce(m.group('c1'), m.group('c2')))
+        report.deaths = int(coalesce(m.group('d1'), m.group('d2')))
+        return True
+
+    m = re.search('se registran un total de '+num_regex('c')+' casos importados confirmados de COVID-19 entre los que se encuentran? '+num_regex('d')+' fallecidos?', text)
+    if m:
+        logging.info("Matched sentence: '{}'".format(m.group(0)))
+        report.cases = int(coalesce(m.group('c1'), m.group('c2')))
+        report.deaths = int(coalesce(m.group('d1'), m.group('d2')))
+        return True
+
+    return False
+
 def parse(text):
     text = text.replace('\n', ' ')
 
     report = Report()
 
-    m = re.search('El total de casos confi?rmados en Argenti?na (a las \d+ horas de hoy )?es de '+num_regex('c')+'( casos)?(\*|\(*\)|, \(\*\))?, (de los cuales )?'+num_regex('d')+' (fallecieron|falleció)', text)
-    if not m:
-        m = re.search('se registran un total de '+num_regex('c')+' casos importados confirmados de COVID-19 entre los que se encuentran? '+num_regex('d')+' fallecidos?', text)
-
-    if not m:
+    if parse_main_totals(text, report):
+        logging.info("Extracted info: cases:{}, deaths:{}".format(report.cases, report.deaths))
+    else:
         logging.warning("No regex matched!")
-
-    if m:
-        logging.info("Matched sentence: '{}'".format(m.group(0)))
-        cases = int(coalesce(m.group('c1'), m.group('c2')))
-        deaths = int(coalesce(m.group('d1'), m.group('d2')))
-        logging.info("Extracted info: cases:{}, deaths:{}".format(cases, deaths))
-
-        report.cases = cases
-        report.deaths = deaths
 
     m = re.search('Hoy fueron confirmados '+num_regex('n')+' nuevos casos de COVID-19', text)
     if m:
